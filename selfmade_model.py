58bff7d16658bfc26341758d9cd6ccf18100cc1a
l1can13 <a.a.soroka05@gmail.com>
2023-05-08 13:03:08 +0400

    movenet added, selfmade started

---
+++ selfmade_model.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+# Загрузка данных
+train_data = tf.data.Dataset.from_tensor_slices((train_images, train_keypoints))
+train_data = train_data.shuffle(buffer_size=1024).batch(batch_size)
+
+# Определение модели
+model = tf.keras.Sequential([
+  layers.Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(128, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(512, activation='relu'),
+  layers.Dense(17) # 17 координат ключевых точек
+])
+
+# Компиляция модели
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.MeanSquaredError(),
+              metrics=['accuracy'])
+
+# Обучение модели
+model.fit(train_data, epochs=10)
+
+# Сохранение модели
+model.save('pose_estimation_model')
