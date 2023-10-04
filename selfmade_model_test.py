0c870bfac80d05745289bfecc03e000dd56d9af2
l1can13 <a.a.soroka05@gmail.com>
2023-10-04 15:08:16 +0400

    trying to make new model pytorch + resnet pretrained

--- selfmade_model_test.py
+++ selfmade_model_test.py
@@ -1,32 +1,37 @@
 import cv2
 import numpy as np
-from keras.utils import img_to_array
+import matplotlib.pyplot as plt
+import tensorflow as tf
 from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 
-# загружаем модель
-model = load_model("my_model.h5")
+# Загрузка обученной модели
+model = load_model('keypoints_model.h5')  # Укажите путь к вашей модели
 
-# загружаем тестовое изображение
-img = cv2.imread("test images/001.jpg")
+# Загрузка тестового изображения
+test_img_path = 'test_images/002.jpg'  # Укажите путь к вашему изображению
+test_img = cv2.imread(test_img_path)
+test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
 
-# преобразуем изображение для подачи в модель
-img = cv2.resize(img, (224, 224))
-img_array = img_to_array(img)
-img_array = img_array.astype('float32') / 255.0
-img_array = np.expand_dims(img_array, axis=0)
+# Изменение размера изображения на 224x224
+test_img = cv2.resize(test_img, (224, 224))
 
-# делаем предсказание на модели
-pred = model.predict(img_array)
+# Подготовка изображения для предсказания моделью
+test_img_array = tf.keras.preprocessing.image.img_to_array(test_img)
+test_img_array = np.expand_dims(test_img_array, axis=0)
+test_img_array = tf.keras.applications.resnet50.preprocess_input(test_img_array)
 
-# преобразуем предсказание в массив координат
-coords = np.squeeze(pred)
+# Получение предсказаний модели
+predictions = model.predict(test_img_array)
 
-# отображаем ключевые точки на изображении
-for i in range(0, len(coords), 2):
-    x, y = int(coords[i]), int(coords[i+1])
-    cv2.circle(img, (x, y), 3, (0, 255, 0), -1)
+predicted_keypoints = decode_predictions(predictions)
 
-# отображаем изображение с ключевыми точками
-cv2.imshow("Keypoints", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Нарисовать ключевые точки на изображении
+for i in range(0, len(predicted_keypoints), 3):
+    x, y, visibility = predicted_keypoints[i], predicted_keypoints[i + 1], predicted_keypoints[i + 2]
+    if visibility > 0.5:  # Рисовать точку, если видимость больше 0.5 (пример)
+        cv2.circle(test_img, (int(x), int(y)), 5, (255, 0, 0), -1)  # Красная точка радиусом 5
+
+# Визуализация результата
+plt.imshow(test_img)
+plt.show()
--- selfmade_model_training.py
+++ selfmade_model_training.py
@@ -1,47 +1,82 @@
-import pandas as pd
-import tensorflow as tf
-from tensorflow.keras import layers, models
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import models, transforms
+from torchvision.datasets import CocoDetection
+from torchvision.models.detection import keypointrcnn_resnet50_fpn
+from torchvision.transforms import functional as F
+from PIL import Image, ImageDraw
+import multiprocessing
 
-# Загрузка данных из файла keypoints_old.csv
-df = pd.read_csv('keypoints.csv')
 
-df.fillna(-1, inplace=True)
+def collate_fn(batch):
+    return tuple(zip(*batch))
 
-# Определение путей к изображениям и ключевым точкам
-image_paths = 'dataset/images/' + df['image'].values
-keypoints = df.iloc[:, 1:].values
 
-# Загрузка изображений и преобразование их в формат, подходящий для обучения модели
-images = []
-for path in image_paths:
-    img = tf.keras.preprocessing.image.load_img(path, grayscale=True, target_size=(128, 128))
-    img = tf.keras.preprocessing.image.img_to_array(img)
-    images.append(img)
-images = tf.stack(images, axis=0) / 255.0
+def train():
+    # Загрузка COCO датасета
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = CocoDetection(
+        root='E:/COCO Dataset/coco2017/val2017',
+        annFile='E:/COCO Dataset/coco2017/annotations/person_keypoints_val2017.json',
+        transform=transform
+    )
 
-output_activation = 'sigmoid'
+    # Создание DataLoader с использованием collate_fn
+    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=collate_fn)
 
-# Разделение данных на обучающую и проверочную выборки
-train_images, val_images = images[:800], images[800:]
-train_keypoints, val_keypoints = keypoints[:800], keypoints[800:]
+    # Загрузка предварительно обученной модели keypointrcnn_resnet50_fpn
+    model = keypointrcnn_resnet50_fpn(pretrained=True)
 
-# Определение архитектуры модели
-model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 1)),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.Flatten(),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(36, activation=output_activation)
-])
+    # Замена последнего слоя классификации
+    num_keypoints = 17  # количество ключевых точек человека
+    in_features = model.roi_heads.keypoint_predictor.kps_score_lowres.in_channels
+    model.roi_heads.keypoint_predictor.kps_score_lowres = nn.ConvTranspose2d(in_features, num_keypoints, 2, 2, 0)
+    model.roi_heads.keypoint_predictor.kps_score_lowres.bias.data.zero_()
+    model.roi_heads.keypoint_predictor.kps_score_lowres.weight.data.zero_()
 
-# Компиляция модели
-model.compile(optimizer='adam', loss='mse')
+    # Обучение модели
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
+    criterion = nn.MSELoss()
 
-# Обучение модели
-history = model.fit(train_images, train_keypoints, epochs=50, batch_size=16, validation_data=(val_images, val_keypoints))
+    num_epochs = 5
 
-# Сохранение обученной модели
-model.save('keypoints_model.h5')
+    for epoch in range(num_epochs):
+        model.train()
+        for images, targets_list in train_loader:
+            images = [image.to(device, dtype=torch.float32) for image in images]
+
+            # Преобразование списка targets в словарь
+            targets = []
+            for target_list in targets_list:
+                if len(target_list) != 0:
+                    for target in target_list:
+                        target_dict = {
+                            'boxes': torch.empty((0, 4), device=device, dtype=torch.long),
+                            'labels': torch.empty((0,), device=device, dtype=torch.long),
+                            'keypoints': torch.tensor(target['keypoints'], device=device, dtype=torch.long),
+                        }
+
+                        print(target_dict)
+
+                        targets.append(target_dict)
+
+            optimizer.zero_grad()
+            loss_dict = model(images, targets)
+            loss = sum(loss for loss in loss_dict.values())
+            loss.backward()
+            optimizer.step()
+
+        print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}")
+
+    # Сохранение модели
+    torch.save(model.state_dict(), 'keypoint_model.pth')
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    multiprocessing.set_start_method('spawn')
+    multiprocessing.Process(target=train).start()
